7d321898e766bee32f7461d27d4979edc4c29293
Sowmya Pasunoori <reddy.pasunoori03@gmail.com>
2026-07-23 18:11:28 -0400

    added  base foundation structure

--- golang/main.go
+++ golang/main.go
@@ -5,8 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
+	"github.com/SowmyaRahul/tyk-sre-assignment/internal/k8s"
 )
 
 func main() {
@@ -15,17 +14,12 @@ func main() {
 
 	flag.Parse()
 
-	kConfig, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	clientset, err := k8s.NewClientset(*kubeconfig)
 	if err != nil {
 		panic(err)
 	}
 
-	clientset, err := kubernetes.NewForConfig(kConfig)
-	if err != nil {
-		panic(err)
-	}
-
-	version, err := getKubernetesVersion(clientset)
+	version, err := k8s.ServerVersion(clientset)
 	if err != nil {
 		panic(err)
 	}
@@ -37,21 +31,7 @@ func main() {
 	}
 }
 
-// getKubernetesVersion returns a string GitVersion of the Kubernetes server defined by the clientset.
-//
-// If it can't connect an error will be returned, which makes it useful to check connectivity.
-func getKubernetesVersion(clientset kubernetes.Interface) (string, error) {
-	version, err := clientset.Discovery().ServerVersion()
-	if err != nil {
-		return "", err
-	}
-
-	return version.String(), nil
-}
-
-// startServer launches an HTTP server with defined handlers and blocks until it's terminated or fails with an error.
-//
-// Expects a listenAddr to bind to.
+// launches an HTTP server with defined handlers 
 func startServer(listenAddr string) error {
 	http.HandleFunc("/healthz", healthHandler)
 
@@ -60,7 +40,7 @@ func startServer(listenAddr string) error {
 	return http.ListenAndServe(listenAddr, nil)
 }
 
-// healthHandler responds with the health status of the application.
+// responds with the health status of the application.
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
